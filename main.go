f48ccbfe0abb188acc12152263c7616ce7e8ec89
Milind Mishra <milind.mishra4@gmail.com>
2024-12-08 00:21:20 +0530

    feat: setup event stream

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 var addr = flag.String("addr", ":3000", "http service address")
@@ -11,9 +12,25 @@ var addr = flag.String("addr", ":3000", "http service address")
 func main() {
 	flag.Parse()
 	http.HandleFunc("/", home)
+	http.HandleFunc("/events", events)
 	http.ListenAndServe(*addr, nil)
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "hello, world")
+}
+
+func events(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/event-stream")
+
+	tokens := []string{"this", "is", "an", "event", "stream", "from", "milind"}
+
+	for _, token := range tokens {
+		content := fmt.Sprintf("data: %s\n\n", string(token))
+		w.Write([]byte(content))
+		w.(http.Flusher).Flush()
+
+		// intentional delay
+		time.Sleep(420 * time.Millisecond)
+	}
 }
